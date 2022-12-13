abedcaa6663f254321cae5fca0c587f708b6dc1b
Dario296 <dariorodriguez296@yahoo.com.ar>
2022-12-12 22:39:40 -0300

    Desafio 8

---
+++ Desafio8/options/sqlite3conn.js
@@ -0,0 +1,9 @@
+const conectionChat = {
+    client: "sqlite3",
+    connection:{
+        filename: "./db/ecommerce.sqlite"
+    },
+    useNullAsDefault: true
+}
+
+module.exports = {conectionChat}
